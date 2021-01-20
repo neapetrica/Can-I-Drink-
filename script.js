1c831f7d67c250f79e99b7116d4d2fc7dba46513
Petrea <77539633+neapetrica@users.noreply.github.com>
2021-01-21 00:53:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function girafa() {
-  Swal.fire("Poti sa bei linistit!");
-}
-function maimuta() {
-	Swal.fire("De ce nu?");
-}
-function elefant() {
-	Swal.fire("Corect!")
-}
-
+function girafa(){
+             Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'De ce are urechile asa mari aceasta girafa?!',
+  footer: '<a href="https://i.pinimg.com/originals/b5/4f/86/b54f86fb4815676d67808065277b6d4f.jpg">Ramai prost daca vezi cum arata o girafa!</a>'
+})
+ }
+ function maimuta(){
+             Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Cam urata maimuta!',
+  footer: '<a href="https://unghiul.com/wp-content/uploads/2017/10/stop-alcohol.jpg">Sigur era apa in paharul ala?</a>'
+})
+ }
+ function elefant(){
+ 	Swal.fire({
+  icon: 'success',
+  title: 'Poti bea linistit in continuare',
+  showConfirmButton: false,
+  timer: 1500
+})
+ }
